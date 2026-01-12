2f50a38934500579b59cc034550a06cdbe2ce899
jdubb <josh.wright.dev@gmail.com>
2026-01-12 11:09:40 -0500

    cleanup default tag logging

--- tags/default/walk.go
+++ tags/default/walk.go
@@ -18,7 +18,7 @@ var TemplateFunctions = template.FuncMap{
 
 func WalkFn(next tag.WalkFn) tag.WalkFn {
 	return func(node *tag.Node) (err error) {
-		if node.Field() == nil { //&& node.Value().Kind() != reflect.Pointer
+		if node.Field() == nil { 
 			return 
 		}
 		if err = text.Parse("default", node, text.LexDefaultWithTemplate); err != nil || node.Skipped(){
@@ -27,8 +27,6 @@ func WalkFn(next tag.WalkFn) tag.WalkFn {
 		for _, value := range node.TagValues() {
 			if value != "" {
 				var _tmpl = template.New("default").Funcs(TemplateFunctions)
-				slog.Info("executing template", "template", value, "data", node.Parent().Value().Interface(),"tag", node.Field().Tag )
-
 				if _tmpl, err = _tmpl.Parse(value); err != nil {
 					return
 				}
@@ -36,19 +34,12 @@ func WalkFn(next tag.WalkFn) tag.WalkFn {
 					slog.Error("default tag template error")
 					return
 				}
-				slog.Info("Node Write", "bytes", node.Bytes())
 				if err = node.UnmarshalText([]byte(node.Bytes())); err != nil {
 					return
 				}
-				slog.Info("Node Unmarshalled", "bytes", node.Value())
-
 				return next(node)
-			}else {
-				slog.Info("Empty tag value", "field", node.Field().Name)
 			}
 		}
-		slog.Info("no default template", "field", node.Field().Name, "tag", node.Field().Tag	)
-
 		return next(node)
 	}
 }
